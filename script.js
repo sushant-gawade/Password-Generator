bc2c1d0493950d450bef2eb63c4053aa2c207692
Sushant Gawade <78841663+sushant-gawade@users.noreply.github.com>
2023-11-11 09:09:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function generatePassword() {
+    var length = 12;
+    var charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_-+=";
+    var password = "";
+    for (var i = 0; i < length; i++) {
+        var randomIndex = Math.floor(Math.random() * charset.length);
+        password += charset.charAt(randomIndex);
+    }
+
+    displayPassword(password);
+}
+
+function displayPassword(password) {
+    var passwordDisplay = document.getElementById("password-display");
+    passwordDisplay.textContent = "Generated Password: " + password;
+}
